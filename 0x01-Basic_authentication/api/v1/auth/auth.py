9611bacfa49306194724e2b78597aab66cff0d56
Chekwasy <richardchekwas@gmail.com>
2024-01-16 23:17:08 +0100

    aa

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""auth file"""
+from flask import request
+
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth method - that returns False - path and excluded_paths
+        will be used later, now, you don’t need to take care of them"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """returns None - request will be the Flask request object"""
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """that returns None - request will be the Flask request object"""
+        return None
