f2ac18380a0ec5ec6752b69a0f57687ba75683d5
Chekwasy <richardchekwas@gmail.com>
2024-01-16 23:27:34 +0100

    aa

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,11 +11,9 @@ class Auth:
         will be used later, now, you don’t need to take care of them"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object"""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """that returns None - request will be the Flask request object"""
