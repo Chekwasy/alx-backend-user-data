f1dce1c4ea481d71f32ddff0fecf945a416a83f0
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-01-20 11:49:28 +0100

    aaa

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Basic Auth class file that inherit from Auth"""
 from api.v1.auth.auth import Auth
+import base64
+from typing import Tuple, TypeVar
 
 
 class BasicAuth(Auth):
@@ -18,3 +20,17 @@ class BasicAuth(Auth):
         if authorization_header[5] != " ":
             return None
         return (authorization_header.split(" "))[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """ decode the autorization header to base64"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except BaseException:
+            return None
