cf38d07063b2644e3c869653cff1fc59035b7c40
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-01-23 20:07:18 +0100

    aaa

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -40,13 +40,12 @@ def filteringrequest():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
-        abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
     if auth.authorization_header(request) is None and auth.session_cookie(
             request) is None:
+        abort(403)
+    if auth.authorization_header(request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
 
