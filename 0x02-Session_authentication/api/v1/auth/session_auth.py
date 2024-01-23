32f99d12c8b7a1e58dac98d3532a4e3d31446319
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-01-23 02:11:41 +0100

    aaa

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Session Auth class to handle session for users and authentication"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """The class begins"""
+    pass
