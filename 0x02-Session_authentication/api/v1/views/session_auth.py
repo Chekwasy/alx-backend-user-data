f08471b5d6e27557b9dbfb398fcb6f5f7746f154
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-01-24 19:23:51 +0100

    aaa

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,55 +4,7 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
-
-
-@app_views.route('/users', methods=['GET'], strict_slashes=False)
-def view_all_users() -> str:
-    """ GET /api/v1/users
-    Return:
-      - list of all User objects JSON represented
-    """
-    all_users = [user.to_json() for user in User.all()]
-    return jsonify(all_users)
-
-
-@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-def view_one_user(user_id: str = None) -> str:
-    """ GET /api/v1/users/:id
-    Path parameter:
-      - User ID
-    Return:
-      - User object JSON represented
-      - 404 if the User ID doesn't exist
-    """
-    if user_id is None:
-        abort(404)
-    if user_id == "me" and request.current_user is None:
-        abort(404)
-    if user_id == "me" and request.current_user is not None:
-        return jsonify(request.current_user.to_json())
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-    return jsonify(user.to_json())
-
-
-@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
-def delete_user(user_id: str = None) -> str:
-    """ DELETE /api/v1/users/:id
-    Path parameter:
-      - User ID
-    Return:
-      - empty JSON is the User has been correctly deleted
-      - 404 if the User ID doesn't exist
-    """
-    if user_id is None:
-        abort(404)
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-    user.remove()
-    return jsonify({}), 200
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -70,44 +22,13 @@ def auth_session_login() -> str:
         userrs = User.search({'email': email})
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
+    if len(userrs) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
     for usr in userrs:
-        if usr.is_valid_password(user_pwd):
+        if usr.is_valid_password(pwd):
             from api.v1.app import auth
             sess = auth.create_session(getattr(usr, "id"))
-            res = jsonify(users[0].to_json())
-            res.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
+            res = jsonify(usr.to_json())
+            res.set_cookie(os.getenv("SESSION_NAME"), sess)
             return res
     return jsonify({"error": "wrong password"}), 401
-
-
-@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-def update_user(user_id: str = None) -> str:
-    """ PUT /api/v1/users/:id
-    Path parameter:
-      - User ID
-    JSON body:
-      - last_name (optional)
-      - first_name (optional)
-    Return:
-      - User object JSON represented
-      - 404 if the User ID doesn't exist
-      - 400 if can't update the User
-    """
-    if user_id is None:
-        abort(404)
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-    rj = None
-    try:
-        rj = request.get_json()
-    except Exception as e:
-        rj = None
-    if rj is None:
-        return jsonify({'error': "Wrong format"}), 400
-    if rj.get('first_name') is not None:
-        user.first_name = rj.get('first_name')
-    if rj.get('last_name') is not None:
-        user.last_name = rj.get('last_name')
-    user.save()
-    return jsonify(user.to_json()), 200
