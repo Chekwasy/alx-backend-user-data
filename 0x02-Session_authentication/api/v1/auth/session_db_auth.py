7da04a878d8d9f0997b44df68fb57ba543f476fa
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-01-26 18:12:54 +0100

    aaa

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -52,7 +52,7 @@ user_id from the database"""
     def destroy_session(self, request=None) -> bool:
         """Destroys an authenticated session.
         """
-         if request is None:
+        if request is None:
             return False
         sess_id = self.session_cookie(request)
         if sess_id is None:
@@ -68,5 +68,5 @@ user_id from the database"""
         if len(sess) <= 0:
             return False
         sess.remove()
-        del self.user_id_by_session_id["sess_id")]
+        del self.user_id_by_session_id["sess_id"]
         return True
