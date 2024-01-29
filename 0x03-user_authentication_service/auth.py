f77b5ea0e5c697f5cc5d5346014043bc5db99c08
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-01-29 22:34:16 +0100

    aaa

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,3 +14,20 @@ def _hash_password(password: str) -> bytes:
     """Hashes a password.
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """method to hash pwd, create user and return user"""
+        hsh_pwd = _hash_password(password)
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, hsh_pwd)
+        raise ValueError("User " + email + " already exists")
